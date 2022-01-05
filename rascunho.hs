735a5742fbe91e102e7a6b22d1145e8b034ddaa8
Luis Antonio <luis.developer.ac@gmail.com>
2022-01-05 16:17:31 -0500

    deletar rascunho

--- rascunho.hs
+++
@@ -1,8 +0,0 @@
-compareInTime :: (Int, Int) -> (Int, Int) -> (Int, Int)
-compareInTime (at, bt) (a, b) 
-    | a > b = (at + 1 , bt)
-    | b > a = (at, bt + 1)
-    | a == b = (at, bt)
-
-compareLists :: [Int] -> [Int] -> (Int, Int) -> (Int, Int)
-compareLists a b (at, bt) = foldl compareInTime (0, 0) $ zip a b
